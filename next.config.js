76fe724f7709fa17bafa0894d50061416977542c
Md Dipul Hasan <mddipulhasan@gmail.com>
2024-10-28 00:17:57 +0600

    update content

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // experimental: {
+  //   // Required:
+  //   appDir: true,
+  // },
+  ...(process.env.NODE_ENV === 'production' && {
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  }),
+
+};
--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['posadmin.expertoftech.com'],
-    },
-  //  reactStrictMode: false, // Disable Strict Mode
-};
-
-export default nextConfig;
